0acaf6bfacc0e08c2a7d4e22cfa3f4f3b50f0d42
liulichao1 <2273360936@qq.com>
2019-06-13 11:32:04 +0800

    Update

---
+++ python_advanced/iterable_test.py
@@ -0,0 +1,23 @@
+class Test(object):
+
+    def __init__(self, n):
+        self.i = 0
+        self.n = n
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.i < self.n:
+            t = self.i
+            self.i = self.i + 1
+            return t
+        else:
+            raise StopIteration('StopIteration...')
+
+
+o = Test(10)
+
+
+for i in o:
+    print(i)
